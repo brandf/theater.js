963caae5586594d57346d328b453eb2d61803124
Brandon Furtwangler <brandon.furtwangler@hotmail.com>
2016-10-20 11:37:23 -0700

    working on getting mocha running as an npm script

--- src/Math/Pose.js
+++ src/Math/Pose.js
@@ -2,9 +2,9 @@ import { mat4, vec3, quat } from 'gl-matrix';
 
 export default class Pose {
   constructor(init) {
-    this.orientation = init.orientation || quat.create();
-    this.position = init.position || vec3.create();
-    this.scale = init.scale || vec3.fromValues(1.0, 1.0, 1.0);
+    this.orientation = (init && init.orientation) || quat.create();
+    this.position = (init && init.position) || vec3.create();
+    this.scale = (init && init.scale) || vec3.fromValues(1.0, 1.0, 1.0);
     this.matrix = mat4.create();
   }
   updateMatrix() {
@@ -18,9 +18,9 @@ export default class Pose {
     vec3.lerp(this.position, p1.position, p2.position, t);
     vec3.lerp(this.scale, p1.scale, p2.scale, t);
   }
-  copyTo(dest) {
-    quat.copy(dest.orientation, this.orientation);
-    vec3.copy(dest.position, this.position);
-    vec3.copy(dest.scale, this.scale);
+  copyFrom(source) {
+    quat.copy(this.orientation, source.orientation);
+    vec3.copy(this.position, source.position);
+    vec3.copy(this.scale, source.scale);
   }
 }
---
+++ src/Math/Pose.test.js
@@ -0,0 +1,61 @@
+import chai from 'chai';
+import Pose from './Pose';
+
+describe('Math/Pose', () => {
+  describe('#constructor', () => {
+    it('has identity orientation', () => {
+
+    });
+    it('has zero position', () => {
+
+    });
+    it('has one scale', () => {
+
+    });
+    it('has identity matrix', () => {
+
+    });
+    it('uses init to override orientation', () => {
+
+    });
+    it('uses init to override position', () => {
+
+    });
+    it('uses init to override scale', () => {
+
+    });
+  });
+  describe('#updateMatrix', () => {
+    it('updates matrix from orientation', () => {
+
+    });
+    it('updates matrix from position', () => {
+
+    });
+    it('updates matrix from scale', () => {
+
+    });
+  });
+  describe('#lerp', () => {
+    it('interpolates orientation', () => {
+
+    });
+    it('interpolates position', () => {
+
+    });
+    it('interpolates scale', () => {
+
+    });
+  });
+  describe('#copyFrom', () => {
+    it('copies orientation', () => {
+
+    });
+    it('copies position', () => {
+
+    });
+    it('copies scale', () => {
+
+    });
+  });
+});
